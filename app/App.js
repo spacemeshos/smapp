2b535241902ce7cddcfa154647168382701c6f8f
Ilya <iliyav@yahoo.com>
2019-02-18 14:09:35 +0200

    New pure react seed

--- app/App.js
+++ app/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { routes } from './vars';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import HomePage from './screens/HomePage';
+import CounterPage from './screens/CounterPage';
 
 class App extends React.Component {
   render() {
