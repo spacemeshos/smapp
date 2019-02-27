aebe9b77c002cf4cacf7cc54adae3ed3d6476631
Ilya <iliyav@yahoo.com>
2019-02-27 15:10:31 +0200

    Set inspect element menu

--- internals/scripts/CheckNodeEnv.js
+++ internals/scripts/CheckNodeEnv.js
@@ -1,13 +1,11 @@
 // @flow
-import chalk from 'chalk';
-
 export default function CheckNodeEnv(expectedEnv: string) {
   if (!expectedEnv) {
     throw new Error('"expectedEnv" not set');
   }
 
   if (process.env.NODE_ENV !== expectedEnv) {
-    console.log(chalk.whiteBright.bgRed.bold(`"process.env.NODE_ENV" must be "${expectedEnv}" to use this webpack config`)); // eslint-disable-line no-console
+    console.log(`"process.env.NODE_ENV" must be "${expectedEnv}" to use this webpack config`); // eslint-disable-line no-console
     process.exit(2);
   }
 }
--- internals/scripts/CheckPortInUse.js
+++ internals/scripts/CheckPortInUse.js
@@ -1,6 +1,5 @@
 // @flow
-import chalk from 'chalk';
-import detectPort from 'detect-port';
+import detectPort from 'detect-port'; // eslint-disable-line import/no-extraneous-dependencies
 
 // eslint-disable-next-line func-names
 (function CheckPortInUse() {
@@ -8,7 +7,7 @@ import detectPort from 'detect-port';
 
   detectPort(port, (err: ?Error, availablePort: number) => {
     if (port !== String(availablePort)) {
-      throw new Error(chalk.whiteBright.bgRed.bold(`Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 yarn dev`));
+      throw new Error(`Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 yarn dev`);
     } else {
       process.exit(0);
     }
