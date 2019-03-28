64c8fad84288b9975ef112de07e8f416f8df887d
admin <admin@admins-MacBook-Pro.local>
2019-03-28 15:34:31 +0200

    local-node-master initial commit

--- app/screens/localNode/LocalNode.js
+++ app/screens/localNode/LocalNode.js
@@ -1,8 +1,7 @@
 // @flow
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
 import styled from 'styled-components';
-import routes from '/routes';
+import { smColors } from '/vars';
 
 const Container = styled.div`
   width: 100%;
@@ -10,19 +9,43 @@ const Container = styled.div`
   padding: 60px 90px;
 `;
 
+const BodyWrapper = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+const Header = styled.span`
+  font-size: 31px;
+  font-weight: bold;
+  color: ${smColors.darkGray};
+`;
+
+const LeftPane = styled.div`
+  text-align: left;
+  flex: 2;
+  padding: 24px 0;
+  margin-right: 32px;
+`;
+
+const RightPane = styled.div`
+  flex: 1;
+  background-color: white;
+  padding: 30px;
+  border: 1px solid ${smColors.borderGray};
+`;
+
 class LocalNode extends Component<{}> {
   render() {
     // TODO: set local node startup status
-    const mode: 'setup' | 'ready' = 'setup';
-    const initialPath = `/main/local-node/local-node${mode === 'setup' ? '-setup' : '-ready'}`;
+    const mode: 'setup' | 'progress' | 'overview' = 'setup';
+    const header = `Local Node${mode !== 'overview' ? ' Setup' : ''}`;
     return (
       <Container>
-        <Switch>
-          {routes.localNode.map((route) => (
-            <Route key={route.path} path={route.path} component={route.component} />
-          ))}
-          <Redirect to={initialPath} />
-        </Switch>
+        <Header>{header}</Header>
+        <BodyWrapper>
+          <LeftPane>Left Pane</LeftPane>
+          <RightPane>Right Pane</RightPane>
+        </BodyWrapper>
       </Container>
     );
   }
