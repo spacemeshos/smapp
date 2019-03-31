51b7d040bc3fccdeb7afccf781b0306140782fc0
admin <admin@admins-MacBook-Pro.local>
2019-03-31 11:56:51 +0300

    linting error fix

--- app/screens/localNode/index.js
+++ app/screens/localNode/index.js
@@ -1,3 +1,1 @@
 export { default as LocalNode } from './LocalNode';
-export { default as LocalNodeLoadingPage } from './LocalNodeLoadingPage';
-export { default as LocalNodePage } from './LocalNodePage';
