925b709b0de00bf92a64275ed689162cc2dbca66
Ilya <iliyav@yahoo.com>
2019-06-12 18:02:39 +0300

    Create new account logic and ui

--- app/screens/wallet/Wallet.js
+++ app/screens/wallet/Wallet.js
@@ -19,6 +19,3 @@ class Wallet extends Component<Props> {
 }
 
 export default Wallet;
-
-// 7be017a967db77fd10ac7c891b3d6d946dea7e3e14756e2f0f9e09b9663f0d9c
-// 81c90dd832e18d1cf9758254327cb3135961af6688ac9c2a8c5d71f73acc5ce57be017a967db77fd10ac7c891b3d6d946dea7e3e14756e2f0f9e09b9663f0d9c
