bbe12fae6e65536eaa64422ce95d06309ae29f20
Travis CI <travis@travis-ci.org>
2019-07-16 15:27:18 +0300

    attempt update auth md from script

--- deployAuthMd.sh
+++ deployAuthMd.sh
@@ -6,6 +6,27 @@ REPO_URL="https://github.com/spacemeshos/testnet-guide.git";
 RELEASE_FOLDER="release";
 AUTH_MD_FILE_NAME="auth.md";
 
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+commit_files() {
+    # test branch
+    git checkout -b test-branch
+    git status;
+    branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
+    echo "BRANCH: $branch";
+    git add .
+    #git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    git commit -am "updating $AUTH_MD_FILE_NAME";
+}
+
+upload_files() {
+   git remote add origin https://${GH_TOKEN}@github.com/spacemeshos/testnet-guide.git > /dev/null 2>&1
+   git push --quiet --set-upstream origin test-branch 
+}
+
 echo "Updating auth.md in $UPLOAD_TARGET_NAME";
 
 echo "Cleanup before clone...";
@@ -13,17 +34,13 @@ rm -rf $REPO_FOLDER_NAME;
 
 git clone $REPO_URL;
 
-# test branch
 cd $REPO_FOLDER_NAME;
-git checkout -b test-branch
 
+setup_git
 cp -v "../$RELEASE_FOLDER/$AUTH_MD_FILE_NAME" "$AUTH_MD_FILE_NAME";
+commit_files
+upload_files
 
-git status;
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
-echo "BRANCH: $branch";
-git add .;
-git commit -am "updating $AUTH_MD_FILE_NAME";
-git push origin "https://${GH_TOKEN}@github.com/spacemeshos/testnet-guide.git";
+# git push origin test-branch;
 
 echo "Done updating $AUTH_MD_FILE_NAME in $UPLOAD_TARGET_NAME.";
