34a8ae0e123c150205d484a8b595e4ac4f80ac0c
yhaspel <yuval3000@gmail.com>
2019-08-28 13:17:24 +0300

    code review changes

--- app/screens/main/Main.js
+++ app/screens/main/Main.js
@@ -140,20 +140,14 @@ class Main extends Component<Props, State> {
     const { activeRouteIndex } = this.state;
     return (
       <Wrapper>
-        <SideBar src={sideBar} />
+        <SideBar src={sideBar} onClick={() => shell.openExternal('https://spacemesh.io')} />
         <NavBar>
           <NavBarPart>
-            <Logo src={logo} onClick={() => shell.openExternal('https://spacemesh.io')} />
+            <Logo src={logo} />
             <NavLinksWrapper>
-              <NavBarLink onClick={() => this.handleNavigation({ index: 0 })} isActive={activeRouteIndex === 0}>
-                MINING
-              </NavBarLink>
-              <NavBarLink onClick={() => this.handleNavigation({ index: 1 })} isActive={activeRouteIndex === 1}>
-                WALLET
-              </NavBarLink>
-              <NavBarLink onClick={() => this.handleNavigation({ index: 2 })} isActive={activeRouteIndex === 2}>
-                CONTACTS
-              </NavBarLink>
+              <NavBarLink onClick={() => this.handleNavigation({ index: 0 })} isActive={activeRouteIndex === 0}>MINING</NavBarLink>
+              <NavBarLink onClick={() => this.handleNavigation({ index: 1 })} isActive={activeRouteIndex === 1}>WALLET</NavBarLink>
+              <NavBarLink onClick={() => this.handleNavigation({ index: 2 })} isActive={activeRouteIndex === 2}>CONTACTS</NavBarLink>
             </NavLinksWrapper>
           </NavBarPart>
           <NavBarPart>
@@ -171,42 +165,15 @@ class Main extends Component<Props, State> {
               <CustomTooltip text="SETTINGS" withIcon={false} />
             </TooltipWrapper>
             <TooltipWrapper>
-              <SecondaryButton
-                onClick={() => this.handleNavigation({ index: 4 })}
-                img={getCoinsIcon}
-                imgHeight={30}
-                imgWidth={30}
-                isPrimary={false}
-                width={35}
-                height={35}
-                style={bntStyle}
-              />
+              <SecondaryButton onClick={() => this.handleNavigation({ index: 4 })} img={getCoinsIcon} imgHeight={30} imgWidth={30} isPrimary={false} width={35} height={35} style={bntStyle} />
               <CustomTooltip text="GET COINS" withIcon={false} />
             </TooltipWrapper>
             <TooltipWrapper>
-              <SecondaryButton
-                onClick={() => this.handleNavigation({ index: 5 })}
-                img={helpIcon}
-                imgHeight={30}
-                imgWidth={30}
-                isPrimary={false}
-                width={35}
-                height={35}
-                style={bntStyle}
-              />
+              <SecondaryButton onClick={() => this.handleNavigation({ index: 5 })} img={helpIcon} imgHeight={30} imgWidth={30} isPrimary={false} width={35} height={35} style={bntStyle} />
               <CustomTooltip text="HELP" withIcon={false} />
             </TooltipWrapper>
             <TooltipWrapper>
-              <SecondaryButton
-                onClick={() => this.handleNavigation({ index: 6 })}
-                img={signOutIcon}
-                imgHeight={30}
-                imgWidth={30}
-                isPrimary={false}
-                width={35}
-                height={35}
-                style={bntStyle}
-              />
+              <SecondaryButton onClick={() => this.handleNavigation({ index: 6 })} img={signOutIcon} imgHeight={30} imgWidth={30} isPrimary={false} width={35} height={35} style={bntStyle} />
               <CustomTooltip text="LOGOUT" withIcon={false} />
             </TooltipWrapper>
           </NavBarPart>
@@ -277,8 +244,7 @@ class Main extends Component<Props, State> {
           logout();
           break;
         }
-        default:
-          break;
+        default: break;
       }
     }
   };
