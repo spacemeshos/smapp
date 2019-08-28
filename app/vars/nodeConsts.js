86ce694bd2adc0275831ae56bf8513e375a7a89a
Ilya <iliyav@yahoo.com>
2019-08-28 15:37:23 +0300

    Integration with node: init mining, mining status, upcoming rewards

--- app/vars/nodeConsts.js
+++ app/vars/nodeConsts.js
@@ -2,7 +2,8 @@ const nodeConsts = {
   COMMITMENT_SIZE: 250, // GB
   NOT_MINING: 1,
   IN_SETUP: 2,
-  IS_MINING: 3
+  IS_MINING: 3,
+  TIME_BETWEEN_LAYERS: 300000
 };
 
 export default nodeConsts;
