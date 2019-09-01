95f9b4d0ba210801cec432144c33f630ac112903
Yuval Haspel <yuval3000@gmail.com>
2019-09-01 13:08:45 +0300

    replace passphrase with password (#180)

--- app/components/settings/index.js
+++ app/components/settings/index.js
@@ -1,4 +1,4 @@
 export { default as SettingsSection } from './SettingsSection';
 export { default as SettingRow } from './SettingRow';
-export { default as ChangePassphrase } from './ChangePassphrase';
+export { default as ChangePassword } from './ChangePassword';
 export { default as SideMenu } from './SideMenu';
