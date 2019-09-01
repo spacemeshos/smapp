a60d29ea31b459e764bb60e57a5e29af96aaf0a6
yhaspel <yuval3000@gmail.com>
2019-09-01 09:23:15 +0300

    button text unify to upper case

--- app/screens/auth/RestoreWallet.js
+++ app/screens/auth/RestoreWallet.js
@@ -31,8 +31,8 @@ class RestoreWallet extends PureComponent<Props> {
         <SideBar src={smallHorizontalSideBar} />
         <SecondaryButton onClick={history.goBack} img={chevronLeftWhite} imgWidth={10} imgHeight={15} style={{ position: 'absolute', bottom: 0, left: -35 }} />
         <Buttons>
-          <Button text="Restore From File" isPrimary={false} onClick={() => history.push('/auth/file-restore')} width={250} style={{ marginBottom: 15 }} />
-          <Button text="Restore With 12 Words" isPrimary={false} onClick={() => history.push('/auth/words-restore')} width={250} />
+          <Button text="RESTORE FROM FILE" isPrimary={false} onClick={() => history.push('/auth/file-restore')} width={250} style={{ marginBottom: 15 }} />
+          <Button text="RESTORE WITH 12 WORDS" isPrimary={false} onClick={() => history.push('/auth/words-restore')} width={250} />
         </Buttons>
         <Link onClick={this.navigateToWalletGuide} text="WALLET GUIDE" style={{ marginTop: 'auto', marginRight: 'auto' }} />
       </CorneredContainer>
