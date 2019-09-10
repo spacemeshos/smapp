80306270c16222b7fefc8a0b39c27ac89815043f
Yuval Haspel <yuval3000@gmail.com>
2019-09-10 18:40:18 +0300

    run packager without publish (#202)
    * run packager without publish
    
    * cleanup
    
    * code review changes
    
    * fixed package script for local usage

--- packagerScript.js
+++ packagerScript.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const util = require('util');
 const crypto = require('crypto');
 
 const { Platform, build } = require('electron-builder');
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-const mkdirAsync = util.promisify(fs.mkdir);
 const args = process.argv.slice(2);
-const targets = args && args.length ? args : null;
-
-if (!targets) {
-  throw new Error("No arguments provided. Usage example: 'node ./packagerScript.js {mac/linux/windows}'");
+if (args.length < 2 || args[0] !== '--target' || !['mac', 'windows', 'linux', 'mwl'].includes(args[1])) {
+  throw new Error("No valid flags provided. Usage example: 'node ./packagerScript.js --target {mac|linux|windows|mwl}'");
 }
+const targets = args[1] === 'mwl' ? ['mac', 'windows', 'linux'] : [args[1]];
+
+if ((args.length > 2 && args.length !== 4) || (args.length === 4 && (args[2] !== '--publish' || !['always', 'never'].includes(args[3])))) {
+  throw new Error("No valid flags provided. Usage example: 'node ./packagerScript.js --target {mac|linux|windows|mwl} --publish {always|never}'");
+}
+const publishFlagValue = args.length === 4 ? args[3] : 'never';
 
 const fileHashList = {
   dmg: {
@@ -45,140 +45,136 @@ const nodeFiles = {
 };
 
 const artifactsToPublishFile = path.join(__dirname, 'release', 'publishFilesList.json');
-
-const generateFileHashFile = async ({ destination }) => {
-  try {
-    const dirname = path.dirname(destination);
-    if (!fs.existsSync(dirname)) {
-      await mkdirAsync(dirname);
-    }
-    if (!fs.existsSync(destination)) {
-      await writeFileAsync(destination, JSON.stringify(fileHashList));
-    }
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
+try {
+  const dirname = path.dirname(artifactsToPublishFile);
+  if (!fs.existsSync(dirname)) {
+    fs.mkdirSync(dirname);
   }
-};
+  if (!fs.existsSync(artifactsToPublishFile)) {
+    fs.writeFileSync(artifactsToPublishFile, JSON.stringify(fileHashList));
+  }
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
 
-generateFileHashFile({ destination: artifactsToPublishFile });
-
-const getFileHash = async ({ filename }) => {
+const getFileHash = ({ filename }) => {
   const shaSum = crypto.createHash('sha512');
-  const fileContent = await readFileAsync(filename);
+  const fileContent = fs.readFileSync(filename);
   const hashSum = shaSum.update(fileContent);
   return hashSum.digest('hex');
 };
 
-const compileHashListFile = async ({ artifactsToPublishFile, artifactPaths }) => {
+const compileHashListFile = ({ artifactsToPublishFile, artifactPaths }) => {
   const acceptedSuffixes = ['dmg', 'exe', 'deb', 'snap', 'AppImage'];
-  let hashList = await readFileAsync(artifactsToPublishFile, 'utf8');
+  let hashList = fs.readFileSync(artifactsToPublishFile, 'utf8');
   for (const fullPath of artifactPaths) {
     const artifactSuffix = fullPath.split('.').pop();
     const artifactName = fullPath.split('/').pop();
     // installers only
     if (acceptedSuffixes.indexOf(artifactSuffix) >= 0) {
-      const installerKey = `${artifactSuffix}_installer`;
-      const shaKey = `${artifactSuffix}_sha512`;
-      const hash = await getFileHash({ filename: fullPath });
+      const hash = getFileHash({ filename: fullPath });
       const artifactNameSpacesReplaced = artifactName.replace(/ /g, '+');
-      hashList = hashList.replace(installerKey, artifactNameSpacesReplaced);
-      hashList = hashList.replace(shaKey, hash);
+      hashList = hashList.replace(`${artifactSuffix}_installer`, artifactNameSpacesReplaced);
+      hashList = hashList.replace(`${artifactSuffix}_sha512`, hash);
     }
   }
-  await writeFileAsync(artifactsToPublishFile, hashList);
+  fs.writeFileSync(artifactsToPublishFile, hashList);
 };
 
-const getBuildOptions = (target) => ({
-  targets: Platform[target.toUpperCase()].createTarget(),
-  publish: 'always',
-  config: {
-    appId: 'com.spacemesh.wallet',
-    productName: 'Spacemesh',
-    files: [
-      'desktop/dist/',
-      'desktop/app.html',
-      'desktop/main.prod.js',
-      'desktop/main.prod.js.map',
-      'desktop/wasm_exec.js',
-      'desktop/ed25519.wasm',
-      'package.json',
-      'node_modules/',
-      'proto/',
-      'resources/icons/*',
-      nodeFiles[target]
-    ],
-    dmg: {
-      window: {
-        width: '350',
-        height: '380'
-      },
-      background: path.join(__dirname, 'resources', 'background.png'),
-      contents: [
-        {
-          x: 300,
-          y: 180,
-          type: 'link',
-          path: '/Applications'
-        },
-        {
-          x: 70,
-          y: 180,
-          type: 'file'
-        }
+const getBuildOptions = ({ target, publish }) => {
+  const buildOptions = {
+    targets: Platform[target.toUpperCase()].createTarget(),
+    publish,
+    config: {
+      appId: 'com.spacemesh.wallet',
+      productName: 'Spacemesh',
+      files: [
+        'desktop/dist/',
+        'desktop/app.html',
+        'desktop/main.prod.js',
+        'desktop/main.prod.js.map',
+        'desktop/wasm_exec.js',
+        'desktop/ed25519.wasm',
+        'package.json',
+        'node_modules/',
+        'proto/',
+        'resources/icons/*',
+        nodeFiles[target]
       ],
-      internetEnabled: true,
-      title: 'Spacemesh',
-      sign: false
-    },
-    win: {
-      target: 'nsis'
-    },
-    nsis: {
-      oneClick: false,
-      perMachine: false,
-      allowElevation: true,
-      allowToChangeInstallationDirectory: true,
-      runAfterFinish: true
-    },
-    linux: {
-      target: ['deb', 'snap', 'AppImage'],
-      category: 'Utility',
-      icon: 'resources/icons'
-    },
-    publish: {
+      dmg: {
+        window: {
+          width: '350',
+          height: '380'
+        },
+        background: path.join(__dirname, 'resources', 'background.png'),
+        contents: [
+          {
+            x: 300,
+            y: 180,
+            type: 'link',
+            path: '/Applications'
+          },
+          {
+            x: 70,
+            y: 180,
+            type: 'file'
+          }
+        ],
+        internetEnabled: true,
+        title: 'Spacemesh',
+        sign: false
+      },
+      win: {
+        target: 'nsis'
+      },
+      nsis: {
+        oneClick: false,
+        perMachine: false,
+        allowElevation: true,
+        allowToChangeInstallationDirectory: true,
+        runAfterFinish: true
+      },
+      linux: {
+        target: ['deb', 'snap', 'AppImage'],
+        category: 'Utility',
+        icon: 'resources/icons'
+      },
+      directories: {
+        buildResources: 'resources',
+        output: 'release'
+      },
+      afterAllArtifactBuild: async (buildResult) => {
+        try {
+          compileHashListFile({ artifactsToPublishFile, artifactPaths: buildResult.artifactPaths });
+          return [artifactsToPublishFile];
+        } catch (error) {
+          console.error(error.message);
+          process.exit(1);
+        }
+      }
+    }
+  };
+
+  if (publish !== 'never') {
+    buildOptions.config.publish = {
       provider: 's3',
       bucket: 'app-binaries.spacemesh.io'
-    },
-    directories: {
-      buildResources: 'resources',
-      output: 'release'
-    },
-    afterAllArtifactBuild: async (buildResult) => {
-      try {
-        await compileHashListFile({ artifactsToPublishFile, artifactPaths: buildResult.artifactPaths });
-        return [artifactsToPublishFile];
-      } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-      }
-    }
+    };
   }
-});
+
+  return buildOptions;
+};
 
 targets.forEach(async (target) => {
-  if (['mac', 'windows', 'linux'].indexOf(target) === -1) {
-    console.error("Invalid target provided. Usage example: 'node ./packagerScript.js {mac/linux/windows}'");
-  } else {
-    try {
-      const res = await build(getBuildOptions(target));
-      if (res && res.length) {
-        console.log(`Artifacts packed for ${target}:`);
-        res.forEach((res, index) => console.log(`${index + 1}. ${res}`));
-      }
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
+  try {
+    const res = await build(getBuildOptions({ target, publish: publishFlagValue }));
+    if (res && res.length) {
+      console.log(`Artifacts packed for ${target}:`);
+      res.forEach((res, index) => console.log(`${index + 1}. ${res}`));
     }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
   }
 });
