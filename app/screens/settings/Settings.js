8616a571cc8ef37bf0567df11a1052bb90ba54d9
yhaspel <yuval3000@gmail.com>
2019-10-10 16:41:58 +0300

    import with destructuring

--- app/screens/settings/Settings.js
+++ app/screens/settings/Settings.js
@@ -14,8 +14,7 @@ import { smColors } from '/vars';
 import type { RouterHistory } from 'react-router-dom';
 import type { Account, Action } from '/types';
 import { localStorageService } from '/infra/storageService';
-
-const packageJson = require('../../../package.json');
+import { version } from '../../../package.json';
 
 const Wrapper = styled.div`
   display: flex;
@@ -174,7 +173,7 @@ class Settings extends Component<Props, State> {
                 upperPartRight={<Link onClick={() => {}} text="CREATE" isDisabled />}
                 rowName="Create a new wallet"
               />
-              <SettingRow upperPartLeft={packageJson.version} upperPartRight={<Link onClick={() => {}} text="CHECK FOR UPDATES" isDisabled />} rowName="Spacemesh Wallet Version" />
+              <SettingRow upperPartLeft={version} upperPartRight={<Link onClick={() => {}} text="CHECK FOR UPDATES" isDisabled />} rowName="Spacemesh Wallet Version" />
             </SettingsSection>
             <SettingsSection title="ACCOUNTS SETTINGS" refProp={this.myRef2}>
               <SettingRow
