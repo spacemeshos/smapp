fd4a2c772f476cf08caab9af49de734f74ee2db4
yhaspel <yuval3000@gmail.com>
2019-10-22 14:58:46 +0300

    fix allocation space issue

--- desktop/diskStorageManager.js
+++ desktop/diskStorageManager.js
@@ -3,11 +3,6 @@ import { ipcConsts, nodeConsts } from '../app/vars';
 
 const si = require('systeminformation');
 
-const convertToGb = (spaceInBytes) => {
-  const i = parseInt(Math.floor(Math.log(spaceInBytes) / Math.log(1024)));
-  return Math.round(spaceInBytes / 1024 ** i);
-};
-
 class DiskStorageManager {
   static getDriveList = async ({ event }) => {
     Promise.all([si.blockDevices(), si.fsSize()]).then(([mountPoints, sizeMountPoints]) => {
@@ -20,7 +15,7 @@ class DiskStorageManager {
         const availableSpace = volume ? volume.size - volume.used : 0;
         if (availableSpace > minimalCommitmentSizeInBytes) {
           const label = (os.type() === 'Darwin' || os.type() === 'Linux') && !!mountPoint.label ? mountPoint.label : mountPoint.name;
-          mappedDrives.push({ mountPoint: mountPoint.mount, label, availableDiskSpace: convertToGb(availableSpace) });
+          mappedDrives.push({ mountPoint: mountPoint.mount, label, availableDiskSpace: Math.round(availableSpace / 1024 ** 3) });
         }
       });
       event.sender.send(ipcConsts.GET_DRIVE_LIST_SUCCESS, mappedDrives);
