2974b4c6eae595c66cd50ddbec04e958af88602d
yhaspel <yuval3000@gmail.com>
2019-10-24 18:07:46 +0300

    code review changes

--- desktop/nodeManager.js
+++ desktop/nodeManager.js
@@ -11,25 +11,23 @@ const osTargetNames = {
   Windows_NT: 'windows'
 };
 
-const getPidByName = ({ name }) => {
-  return new Promise((resolve, reject) => {
-    try {
-      find('name', name).then((list) => {
-        resolve(list?.length && list[0].pid ? list[0].pid : null);
-      });
-    } catch {
-      reject();
-    }
-  });
+const getPidByName = async ({ name }) => {
+  try {
+    const list = await find('name', name);
+    return list.length && list[0].pid ? list[0].pid : null;
+  } catch {
+    return null;
+  }
 };
 
 class NodeManager {
   static startNode = async ({ event }) => {
-    const isDevMode = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+    const isDevMode = process.env.NODE_ENV === 'development';
     if (Object.keys(osTargetNames).indexOf(os.type()) < 0) {
       event.sender.send(ipcConsts.START_NODE_FAILURE, 'OS not supported.');
     }
     const osTarget = osTargetNames[os.type()];
+    // TODO: remove this and change to binary file path when dev mode uses binary node file
     const devPath = './miniMesh.sh';
     // TODO: should change prodPath to actual executable file path in prod.
     const prodPath = path.resolve(`${process.resourcesPath}/../node/${osTarget}/${osTarget === 'windows' ? '' : osTarget}go-spacemesh${osTarget === 'windows' ? '.exe' : ''}`);
@@ -46,7 +44,8 @@ class NodeManager {
 
   static killNodeProcess = async ({ event }) => {
     try {
-      const isDevMode = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+      const isDevMode = process.env.NODE_ENV === 'development';
+      // TODO: remove this when dev mode uses binary node file
       if (isDevMode) {
         event.sender.send(ipcConsts.QUIT_NODE_SUCCESS);
       } else {
