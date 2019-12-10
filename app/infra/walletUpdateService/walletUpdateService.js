6975ffec3497fd589086ce3974fb8dbca67f097f
Yuval Haspel <yuval3000@gmail.com>
2019-12-10 16:00:55 +0200

    App update settings page (#287)
    * check update from settings page
    
    * cleanup
    
    * code review changes
    
    * code review changes - wallet update state in redux
    
    * code review changes
    
    * code review changes
    
    * code review changes
    
    * cleanup
    
    * code review changes
    
    * code review changes
    
    * remove update ready from redux
    
    * code revew changes

--- app/infra/walletUpdateService/walletUpdateService.js
+++ app/infra/walletUpdateService/walletUpdateService.js
@@ -7,24 +7,22 @@ import { notificationsService } from '/infra/notificationsService';
 class WalletUpdateService {
   static checkForWalletUpdate() {
     ipcRenderer.send(ipcConsts.CHECK_WALLET_UPDATE);
-    return new Promise<string, Error>((resolve: Function) => {
-      ipcRenderer.once(ipcConsts.CHECK_WALLET_UPDATE_SUCCESS, (event, xml) => {
-        listenerCleanup({ ipcRenderer, channels: [ipcConsts.CHECK_WALLET_UPDATE_SUCCESS] });
-        resolve(xml);
-      });
-    });
   }
 
   static listenToUpdaterError({ onUpdaterError }: { onUpdaterError: () => void }) {
     ipcRenderer.once(ipcConsts.WALLET_UPDATE_ERROR, () => {
-      listenerCleanup({ ipcRenderer, channels: [ipcConsts.CHECK_WALLET_UPDATE_SUCCESS, ipcConsts.WALLET_UPDATE_ERROR, ipcConsts.DOWNLOAD_UPDATE_COMPLETED] });
+      listenerCleanup({ ipcRenderer, channels: [ipcConsts.WALLET_UPDATE_ERROR, ipcConsts.DOWNLOAD_UPDATE_PROGRESS, ipcConsts.DOWNLOAD_UPDATE_COMPLETED] });
       onUpdaterError();
     });
   }
 
-  static listenToDownloadUpdate({ onDownloadUpdateCompleted }: { onDownloadUpdateCompleted: () => void }) {
-    ipcRenderer.once(ipcConsts.DOWNLOAD_UPDATE_COMPLETED, () => {
-      listenerCleanup({ ipcRenderer, channels: [ipcConsts.DOWNLOAD_UPDATE_COMPLETED] });
+  static listenToDownloadUpdate({ onDownloadUpdateCompleted, onDownloadProgress }: { onDownloadUpdateCompleted: () => void, onDownloadProgress: () => void }) {
+    ipcRenderer.on(ipcConsts.DOWNLOAD_UPDATE_PROGRESS, () => {
+      listenerCleanup({ ipcRenderer, channels: [ipcConsts.DOWNLOAD_UPDATE_PROGRESS] });
+      onDownloadProgress();
+    });
+    ipcRenderer.on(ipcConsts.DOWNLOAD_UPDATE_COMPLETED, () => {
+      listenerCleanup({ ipcRenderer, channels: [ipcConsts.DOWNLOAD_UPDATE_COMPLETED, ipcConsts.DOWNLOAD_UPDATE_PROGRESS] });
       notificationsService.notify({
         title: 'Spacemesh',
         notification: 'An important update is available.'
