f6c8668dbb7f7c54f8129f64c141569e5f74cf86
Ilya <iliyav@yahoo.com>
2020-09-17 11:29:52 +0300

    PoST API

--- app/vars/nodeConsts.js
+++ app/vars/nodeConsts.js
@@ -2,9 +2,7 @@ const nodeConsts = {
   MINING_UNSET: 0,
   NOT_MINING: 1,
   IN_SETUP: 2,
-  IS_MINING: 3,
-  DEFAULT_URL: 'localhost:9091',
-  ADDRESS_LENGTH: 20
+  IS_MINING: 3
 };
 
 export default nodeConsts;
---
+++ desktop/netServiceFactory.js
@@ -0,0 +1,16 @@
+import path from 'path';
+
+const protoLoader = require('@grpc/proto-loader');
+
+const grpc = require('grpc');
+
+class NetServiceFactory {
+  constructor(protoPath, url, serviceName) {
+    const resolvedProtoPath = path.join(__dirname, '..', protoPath);
+    const packageDefinition = protoLoader.loadSync(resolvedProtoPath);
+    const proto = grpc.loadPackageDefinition(packageDefinition);
+    this.service = new proto.pb[serviceName](url, grpc.credentials.createInsecure());
+  }
+}
+
+export default NetServiceFactory;
