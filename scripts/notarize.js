5e5d532a3e774963b4724367e9a6ff7406c48c6a
Ilya <iliyav@yahoo.com>
2020-10-28 18:14:47 +0200

    Changed flow.js to typescript

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import { notarize } from 'electron-notarize';
+const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
