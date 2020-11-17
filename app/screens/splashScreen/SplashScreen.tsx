c5fdcddc8388d658f38be74b3d2ae6127b333133
Alexander Molotsylo <motzart66@gmail.com>
2020-11-17 18:06:13 +0200

    Spinning logo forever when app fails to connect to node #575 (#623)
    * Spinning logo forever when app fails to connect to node #575
    
    * Updated to ts version

--- app/screens/splashScreen/SplashScreen.tsx
+++ app/screens/splashScreen/SplashScreen.tsx
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import { RouteComponentProps } from 'react-router-dom';
 import { getNodeStatus } from '../../redux/node/actions';
+import { Modal } from '../../components/common';
 import { Loader } from '../../basicComponents';
 import { eventsService } from '../../infra/eventsService';
 import { RootState } from '../../types';
 
+const Message = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 30px 0 150px 0;
+`;
+
 const SplashScreen = ({ history }: RouteComponentProps) => {
+  const [showErrorModal, setShowErrorModal] = useState(false);
   const isDarkMode = useSelector((state: RootState) => state.ui.isDarkMode);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    let interval: any = null;
+    let interval: number;
     const startNode = async () => {
       const status = await dispatch(getNodeStatus());
       // @ts-ignore
@@ -23,11 +33,15 @@ const SplashScreen = ({ history }: RouteComponentProps) => {
         if (isReady) {
           history.push('/auth');
         } else {
+          const startTime = new Date().getTime();
           interval = setInterval(async () => {
             const isReady = await eventsService.isServiceReady();
             if (isReady) {
               clearInterval(interval);
               history.push('/auth');
+            } else {
+              const endTime = startTime + 180000 > new Date().getTime();
+              !endTime && setShowErrorModal(true);
             }
           }, 200);
         }
@@ -39,7 +53,13 @@ const SplashScreen = ({ history }: RouteComponentProps) => {
     };
   });
 
-  return <Loader size={Loader.sizes.BIG} isDarkMode={isDarkMode} />;
+  return showErrorModal ? (
+    <Modal header="Error" subHeader="Something`s wrong here...">
+      <Message>Restart app please...</Message>
+    </Modal>
+  ) : (
+    <Loader size={Loader.sizes.BIG} isDarkMode={isDarkMode} />
+  );
 };
 
 export default SplashScreen;
