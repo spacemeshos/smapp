e0b929635180449229ada3c9cfdd04598526dd8b
Alex <motzart66@gmail.com>
2020-12-18 12:40:06 +0200

    update Network Screen

---
+++ app/basicComponents/CustomTimeAgo.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import TimeAgo from 'react-timeago';
+
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
+
+const enString = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: 'ago',
+  suffixFromNow: 'from now',
+  seconds: 'few secs',
+  minute: 'a min',
+  minutes: '%d min',
+  hour: '1 hr',
+  hours: '%d hrs',
+  day: '1 day',
+  days: '%d days',
+  month: '1 month',
+  months: '%d months',
+  year: '1 year',
+  years: '%d years',
+  wordSeparator: ' '
+};
+
+type Props = {
+  time: number;
+};
+
+const CustomTimeAgo = ({ time }: Props) => {
+  const jsDate = new Date(time).getTime();
+
+  const formatter = buildFormatter(enString);
+  return <TimeAgo date={jsDate} formatter={formatter} />;
+};
+
+export default CustomTimeAgo;
