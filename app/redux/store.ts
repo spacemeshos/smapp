4f625c381009bffa97fdf9ce38da58f1b7f83dd3
Kirill Shumilov <brusher.designer@gmail.com>
2021-08-13 03:28:49 +0300

    Get rid of @ts-ignore in store.ts

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -3,9 +3,14 @@ import thunk from 'redux-thunk';
 import { CustomAction, RootState, AppThDispatch, GetState } from '../types';
 import createRootReducer from './rootReducer';
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
 const rootReducer = createRootReducer();
 
-// @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store: Store<RootState, CustomAction> & { dispatch: AppThDispatch; getState: GetState } = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
