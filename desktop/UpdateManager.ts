848ac3eee929b9554106be0ae8031e863316ce2f
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-09-13 18:25:10 +0100

    fix(updatemanager): ensure last prompted version is not notified again

--- desktop/UpdateManager.ts
+++ desktop/UpdateManager.ts
@@ -4,25 +4,25 @@ import log from 'electron-log';
 import StoreService from './storeService';
 import { MessageBoxOptions } from 'electron/main';
 import NotificationManager from './notificationManager';
+import compareVersions from 'compare-versions';
 export default class UpdateManager {
   private mainWindow: BrowserWindow;
   private notification: NotificationManager;
+
+  private lastVersionPrompted: string = StoreService.get('userSettings.latestVersionPrompted');
 
   UPDATE_INTERVAL = 1000 * 60 * 60 * 24;
 
   constructor(mainWindow: BrowserWindow) {
     this.mainWindow = mainWindow;
     this.notification = new NotificationManager(this.mainWindow);
-    if (process.env.NODE_ENV === 'production' && StoreService.get('userSettings.promptForUpdate')) {
+
+    if (process.env.NODE_ENV === 'production') {
       log.transports.file.level = 'info';
       autoUpdater.logger = log;
 
       autoUpdater.channel = 'latest';
       autoUpdater.allowDowngrade = false;
-
-      this.checkForUpdates();
-
-      autoUpdater.on('update-downloaded', this.onUpdateAvailable);
 
       setInterval(() => autoUpdater.checkForUpdates(), this.UPDATE_INTERVAL);
     }
@@ -34,19 +34,22 @@ export default class UpdateManager {
 
   async checkForUpdates() {
     try {
-      const { downloadPromise } = await autoUpdater.checkForUpdates();
+      const { downloadPromise, updateInfo } = await autoUpdater.checkForUpdates();
       if (!downloadPromise) {
         dialog.showMessageBox({
           type: 'info',
           message: 'There are currently no updates available.'
         });
       }
+      if (typeof this.lastVersionPrompted === 'undefined' || compareVersions(this.lastVersionPrompted, updateInfo.version) > 1) {
+        autoUpdater.on('update-downloaded', () => this.onUpdateAvailable(updateInfo.version));
+      }
     } catch (error: unknown) {
       this.notification.showNotification({ title: 'Error checking for updates', body: 'Check your internet connection' });
     }
   }
 
-  async onUpdateAvailable() {
+  async onUpdateAvailable(version: string) {
     const options: MessageBoxOptions = {
       message: `An important App update is available. \n\tWould you like to update now?`,
       type: 'question',
@@ -56,7 +59,7 @@ export default class UpdateManager {
     if (response === 0) {
       this.updateApplication();
     } else if (response === 1) {
-      StoreService.set('userSettings.promptForUpdate', false);
+      StoreService.set('userSettings.latestVersionPrompted', version);
     }
   }
 }
