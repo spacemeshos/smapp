9d1c1249b0b63805160e420effadd8c2433f591a
brusher_ru <brusher.designer@gmail.com>
2022-04-12 15:01:37 +0700

    Fix(unlock): unlock wallet while switch network is required

--- app/routeUtils.ts
+++ app/routeUtils.ts
@@ -1,8 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 
+import { useHistory } from 'react-router';
 import { AuthPath } from './routerPaths';
 
-export const goToSwitchNetwork = (history, isWalletOnly) =>
+export const goToSwitchNetwork = (history: ReturnType<typeof useHistory>, isWalletOnly: boolean) =>
   setImmediate(() => {
+    if (history.location.pathname === AuthPath.SwitchNetwork) return;
     history.push(AuthPath.SwitchNetwork, { redirect: history.location.pathname, isWalletOnly });
   });
