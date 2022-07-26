b16cdff1e78b7343f8abe0fbb472796671d8a6d1
maxym <maksim.parfenov@protonmail.ch>
2022-07-26 14:11:25 +0100

    feat: replace auto-launch with native electron API and start node before unlock wallet

--- desktop/main/NodeConfig.ts
+++ desktop/main/NodeConfig.ts
@@ -1,15 +1,14 @@
-import { promises as fs } from 'fs';
+import { existsSync, promises as fs } from 'fs';
 import { NodeConfig } from '../../shared/types';
-import { fetchJSON, isFileExists } from '../utils';
+import { fetchJSON } from '../utils';
 import { NODE_CONFIG_FILE } from './constants';
 
-export const loadNodeConfig = () =>
-  fs.readFile(NODE_CONFIG_FILE, { encoding: 'utf8' }).then(JSON.parse);
-
-const loadSmeshingOpts = async () =>
-  (await isFileExists(NODE_CONFIG_FILE))
-    ? (await loadNodeConfig()).smeshing
+export const loadNodeConfig = async () =>
+  existsSync(NODE_CONFIG_FILE)
+    ? fs.readFile(NODE_CONFIG_FILE, { encoding: 'utf8' }).then(JSON.parse)
     : {};
+
+const loadSmeshingOpts = async () => (await loadNodeConfig()).smeshing;
 
 export const downloadNodeConfig = async (networkConfigUrl: string) => {
   const nodeConfig = await fetchJSON(networkConfigUrl);
