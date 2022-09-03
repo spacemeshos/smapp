09517fd4bf122bb7f75df7ceabb074b3bd83b651
maxym <maksim.parfenov@protonmail.ch>
2022-09-03 14:48:31 +0100

    fix: replace native auto-launch with npm module

--- desktop/auto-launch.ts
+++ desktop/auto-launch.ts
@@ -1,3 +1,4 @@
+import os from 'os';
 import { existsSync } from 'fs';
 import { app, ipcMain } from 'electron';
 import { ipcConsts } from '../app/vars';
@@ -7,10 +8,23 @@ import { NODE_CONFIG_FILE } from './main/constants';
 const IS_AUTO_START_ENABLED = 'isAutoStartEnabled';
 
 export const getWasOpenAtLaunchValue = () => {
+  const platform = os.platform();
   const isConfigFileExists = existsSync(NODE_CONFIG_FILE);
 
   if (!isConfigFileExists) {
     return false;
+  }
+
+  if (platform === 'darwin') {
+    return app.getLoginItemSettings().wasOpenedAtLogin;
+  }
+
+  if (platform === 'linux') {
+    return StoreService.get(IS_AUTO_START_ENABLED);
+  }
+
+  if (platform === 'win32') {
+    return app.getLoginItemSettings().executableWillLaunchAtLogin;
   }
 
   return (
