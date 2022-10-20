e0c64c1e59d3181092e5d3e927df4894c496e189
maxym <maksim.parfenov@protonmail.ch>
2022-10-20 14:02:20 +0100

    feat: add tests

--- tests/create-wallet.spec.ts
+++ tests/create-wallet.spec.ts
@@ -1,236 +1,43 @@
+import '../desktop/wasm_exec';
+
+import path from 'path';
+import fs from 'fs';
 import CryptoService from '../desktop/cryptoService';
 
-beforeAll(() => {
-  Object.defineProperty(global, '__generateKeyPair', {
-    value: (...props) => {
-      const saveKeys = props[1];
+beforeAll(async () => {
+  const bytes = fs.readFileSync(
+    path.resolve(__dirname, '../desktop', 'ed25519.wasm')
+  );
+  // @ts-ignore
+  const go = new Go(); // eslint-disable-line no-undef
+  const { instance } = await WebAssembly.instantiate(bytes, go.importObject);
 
-      saveKeys(
-        [
-          32,
-          93,
-          141,
-          78,
-          69,
-          139,
-          22,
-          61,
-          91,
-          161,
-          90,
-          199,
-          18,
-          149,
-          29,
-          86,
-          89,
-          204,
-          81,
-          55,
-          158,
-          126,
-          10,
-          209,
-          58,
-          204,
-          151,
-          48,
-          58,
-          168,
-          80,
-          147,
-        ],
-        [
-          102,
-          157,
-          9,
-          17,
-          149,
-          249,
-          80,
-          230,
-          37,
-          90,
-          46,
-          135,
-          120,
-          238,
-          167,
-          190,
-          79,
-          122,
-          102,
-          175,
-          232,
-          85,
-          149,
-          116,
-          4,
-          236,
-          21,
-          32,
-          200,
-          161,
-          31,
-          241,
-          32,
-          93,
-          141,
-          78,
-          69,
-          139,
-          22,
-          61,
-          91,
-          161,
-          90,
-          199,
-          18,
-          149,
-          29,
-          86,
-          89,
-          204,
-          81,
-          55,
-          158,
-          126,
-          10,
-          209,
-          58,
-          204,
-          151,
-          48,
-          58,
-          168,
-          80,
-          147,
-        ]
-      );
-    },
-  });
-
-  Object.defineProperty(global, '__deriveNewKeyPair', {
-    value: (...props) => {
-      const saveKeys = props[3];
-
-      saveKeys(
-        [
-          32,
-          93,
-          141,
-          78,
-          69,
-          139,
-          22,
-          61,
-          91,
-          161,
-          90,
-          199,
-          18,
-          149,
-          29,
-          86,
-          89,
-          204,
-          81,
-          55,
-          158,
-          126,
-          10,
-          209,
-          58,
-          204,
-          151,
-          48,
-          58,
-          168,
-          80,
-          147,
-        ],
-        [
-          102,
-          157,
-          9,
-          17,
-          149,
-          249,
-          80,
-          230,
-          37,
-          90,
-          46,
-          135,
-          120,
-          238,
-          167,
-          190,
-          79,
-          122,
-          102,
-          175,
-          232,
-          85,
-          149,
-          116,
-          4,
-          236,
-          21,
-          32,
-          200,
-          161,
-          31,
-          241,
-          32,
-          93,
-          141,
-          78,
-          69,
-          139,
-          22,
-          61,
-          91,
-          161,
-          90,
-          199,
-          18,
-          149,
-          29,
-          86,
-          89,
-          204,
-          81,
-          55,
-          158,
-          126,
-          10,
-          209,
-          58,
-          204,
-          151,
-          48,
-          58,
-          168,
-          80,
-          147,
-        ]
-      );
-    },
-  });
+  go.run(instance);
 });
 
 describe('create wallet', () => {
+  const pubKey =
+    '205d8d4e458b163d5ba15ac712951d5659cc51379e7e0ad13acc97303aa85093';
+  const pKey =
+    '669d091195f950e6255a2e8778eea7be4f7a66afe855957404ec1520c8a11ff1205d8d4e458b163d5ba15ac712951d5659cc51379e7e0ad13acc97303aa85093';
   const mnemonic =
     'film theme cheese broken kingdom destroy inch ready wear inspire shove pudding';
   it('match keys from mnemonic', () => {
     const result = CryptoService.createWallet(mnemonic);
-    const pubKey =
-      '205d8d4e458b163d5ba15ac712951d5659cc51379e7e0ad13acc97303aa85093';
-    const pKey =
-      '669d091195f950e6255a2e8778eea7be4f7a66afe855957404ec1520c8a11ff1205d8d4e458b163d5ba15ac712951d5659cc51379e7e0ad13acc97303aa85093';
     expect(result.address).toEqual(pubKey);
     expect(result.secretKey).toEqual(pKey);
     expect(result.publicKey).toEqual(pubKey);
+  });
+
+  it('sign message', async () => {
+    const message = 'hello world';
+    const signature: string = await CryptoService.signMessage({
+      message,
+      secretKey: pKey,
+    });
+
+    expect(signature).toEqual(
+      '164c808da28f84bd4939a77e9584e27c111885634ea1e0a2018b7a2ab8dbe19c5b01535691be2c4c198984edfced8d9e7aab4c9300647725629273e2ca8eae01'
+    );
   });
 });
