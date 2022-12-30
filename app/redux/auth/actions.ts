340d5e62bae874c33090242171ed7444adb7a450
brusher_ru <brusher.designer@gmail.com>
2022-12-30 15:50:38 -0500

    fix: reset wallet in main process on logout

--- app/redux/auth/actions.ts
+++ app/redux/auth/actions.ts
@@ -1,7 +1,9 @@
+import { eventsService } from '../../infra/eventsService';
 import { CustomAction } from '../../types';
 
 export const LOGOUT = 'LOGOUT';
 
 export const logout = (): CustomAction => {
+  eventsService.closeWallet();
   return { type: LOGOUT };
 };
