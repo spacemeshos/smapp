5b7445eee05364c01d66beb5816255024e7e8120
brusher_ru <brusher.designer@gmail.com>
2023-01-26 19:02:13 -0500

    tweak: show "Genesis will end in N hours" in genesis phase instead of Current/Verified layers

--- app/redux/network/selectors.ts
+++ app/redux/network/selectors.ts
@@ -1,3 +1,7 @@
+import {
+  firstLayerInEpoch,
+  timestampByLayer,
+} from '../../../shared/layerUtils';
 import { RootState } from '../../types';
 
 export const getNetworkInfo = (state: RootState) => state.network;
@@ -7,3 +11,9 @@ export const getNetworkName = (state: RootState) =>
 
 export const getGenesisID = (state: RootState) =>
   getNetworkInfo(state).genesisID;
+
+export const getTimestampByLayerFn = (state: RootState) =>
+  timestampByLayer(state.network.genesisTime, state.network.layerDurationSec);
+
+export const getFirstLayerInEpochFn = (state: RootState) =>
+  firstLayerInEpoch(state.network.layersPerEpoch);
