b4dfe584ad10f32c81a96035dbf3e40ee29421f5
brusher_ru <brusher.designer@gmail.com>
2023-03-29 18:18:08 -0600

    feat: add force autoupdate feature

--- desktop/main/sources/autoUpdate.ts
+++ desktop/main/sources/autoUpdate.ts
@@ -1,6 +1,7 @@
 import { BrowserWindow } from 'electron';
 import { UpdateInfo } from 'electron-updater';
 import {
+  combineLatest,
   distinctUntilChanged,
   filter,
   first,
@@ -14,18 +15,25 @@ import {
 } from 'rxjs';
 import { ipcConsts } from '../../../app/vars';
 import { Network } from '../../../shared/types';
+import { delay } from '../../../shared/utils';
+import Logger from '../../logger';
+import { Managers } from '../app.types';
 import {
   checkUpdates,
+  getCurrentVersion,
   installUpdate,
   notifyDownloadStarted,
   subscribe,
   unsubscribe,
 } from '../autoUpdater';
-import { HOUR } from '../constants';
+import { HOUR, MINUTE } from '../constants';
 import { fromIPC } from '../rx.utils';
+
+const logger = Logger({ className: 'autoUpdate' });
 
 const handleAutoUpdates = (
   $mainWindow: Observable<BrowserWindow>,
+  $managers: Observable<Managers>,
   $currentNetwork: Observable<Network | null>
 ) => {
   type Data = [BrowserWindow, Network, boolean];
@@ -58,10 +66,13 @@ const handleAutoUpdates = (
 
   const subs = [
     // Each time we got new mainWindow - resubscribe
-    $mainWindow.subscribe((mainWnindow) => {
-      unsubscribe();
-      subscribe(mainWnindow, $downloaded);
-    }),
+    combineLatest([$mainWindow, $currentNetwork]).subscribe(
+      ([mainWnindow, currentNetwork]) => {
+        if (!currentNetwork) return;
+        unsubscribe();
+        subscribe(mainWnindow, currentNetwork, $downloaded);
+      }
+    ),
     // Check for updates when: init, ipc request, daily
     $trigger.subscribe(async ([mainWindow, curNet, download]) => {
       const nextUpdateInfo = await checkUpdates(mainWindow, curNet, download);
@@ -70,6 +81,36 @@ const handleAutoUpdates = (
         notifyDownloadStarted(mainWindow);
       }
     }),
+    // Force update if needed
+    combineLatest([$managers, $downloaded, $currentNetwork])
+      .pipe(
+        map(([managers, updateInfo, curNetwork]) => {
+          if (curNetwork?.minSmappRelease) {
+            const currentVersion = getCurrentVersion();
+            const isOutdatedVersion =
+              currentVersion.compare(curNetwork.minSmappRelease) === -1;
+
+            if (!isOutdatedVersion) return null;
+
+            return async () => {
+              try {
+                logger.log('forceUpdate', updateInfo);
+                await managers.node.stopNode();
+                installUpdate();
+                // In case if something failed we need to start Node back
+              } catch (err) {
+                logger.error('forceUpdate', err);
+              }
+              await delay(MINUTE);
+              const res = await managers.node.startNode();
+              logger.log('forceUpdate -> recover Node', res);
+            };
+          }
+          return null;
+        }),
+        filter(Boolean)
+      )
+      .subscribe((runForceUpdate) => runForceUpdate()),
     // Trigger check of updates
     fromIPC<void>(ipcConsts.AU_CHECK_UPDATES).subscribe(() =>
       $request.next(false)
