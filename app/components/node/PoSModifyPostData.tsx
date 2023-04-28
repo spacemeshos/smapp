11ed3e75b2dd9dd119f934b10cb0943a002e5b2b
maxym <maksim.parfenov@protonmail.ch>
2023-04-28 12:41:35 +0300

    feat(): disable smeshing actions if node is down

--- app/components/node/PoSModifyPostData.tsx
+++ app/components/node/PoSModifyPostData.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import { RootState } from 'app/types';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Tooltip, Button } from '../../basicComponents';
+import { Modal } from '../common';
 
 const Wrapper = styled.div`
   display: flex;
@@ -38,24 +41,54 @@ const Dots = styled.div`
   color: ${({ theme: { color } }) => color.primary};
 `;
 
+const StyledRow = styled(Row)`
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
+`;
+
 type Props = {
   deleteData: () => void;
 };
 
-const PoSModifyPostData = ({ deleteData }: Props) => (
-  <>
-    <Wrapper>
-      <Row>
-        <Text>Stop smeshing and delete PoS data</Text>
-        <Tooltip
-          width={200}
-          text="Stop Smeshing and delete the POS data files."
-        />
-        <Dots>.....................................................</Dots>
-        <Button onClick={deleteData} text="DELETE DATA" isPrimary={false} />
-      </Row>
-    </Wrapper>
-  </>
-);
+const PoSModifyPostData = ({ deleteData }: Props) => {
+  const nodeError = useSelector((state: RootState) => state.node.error);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const onDeleteClick = () => {
+    setShowModal(true);
+  };
+  return (
+    <>
+      <Wrapper>
+        <Row>
+          <Text>Stop smeshing and delete PoS data</Text>
+          <Tooltip
+            width={200}
+            text="Stop Smeshing and delete the POS data files."
+          />
+          <Dots>.....................................................</Dots>
+          <Button
+            isDisabled={!!nodeError}
+            onClick={onDeleteClick}
+            text="DELETE DATA"
+            isPrimary={false}
+          />
+          {showModal && (
+            <Modal header="Are you sure you want to delete your POS data?">
+              <StyledRow>
+                <Button
+                  isPrimary={false}
+                  text="CANCEL"
+                  onClick={() => setShowModal(false)}
+                />
+                <Button text="CONFIRM" onClick={() => deleteData()} />
+              </StyledRow>
+            </Modal>
+          )}
+        </Row>
+      </Wrapper>
+    </>
+  );
+};
 
 export default PoSModifyPostData;
