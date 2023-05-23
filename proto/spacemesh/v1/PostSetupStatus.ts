14af079ae559dec14143145addcbce293cbd6c73
brusher_ru <brusher.designer@gmail.com>
2023-05-23 11:30:37 +0400

    fix: add missing PostSetupStatus state

--- proto/spacemesh/v1/PostSetupStatus.ts
+++ proto/spacemesh/v1/PostSetupStatus.ts
@@ -8,10 +8,11 @@ import type { Long } from '@grpc/proto-loader';
 export enum _spacemesh_v1_PostSetupStatus_State {
   STATE_UNSPECIFIED = 0,
   STATE_NOT_STARTED = 1,
-  STATE_IN_PROGRESS = 2,
-  STATE_PAUSED = 3,
-  STATE_COMPLETE = 4,
-  STATE_ERROR = 5,
+  STATE_PREPARED = 2,
+  STATE_IN_PROGRESS = 3,
+  STATE_PAUSED = 4,
+  STATE_COMPLETE = 5,
+  STATE_ERROR = 6,
 }
 
 export interface PostSetupStatus {
