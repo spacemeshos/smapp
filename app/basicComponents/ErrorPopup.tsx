3436991303b4c5a12a08a7fde4a1209df3efbc45
maxym <maksim.parfenov@protonmail.ch>
2023-07-12 18:04:26 +0300

    feat_1310: fix sending to same addreess

--- app/basicComponents/ErrorPopup.tsx
+++ app/basicComponents/ErrorPopup.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { closePopup } from '../assets/images';
 
-const Wrapper = styled.div<{ onClick: (e?: React.MouseEvent) => void }>`
+const Wrapper = styled.div<{
+  onClick: (e?: React.MouseEvent) => void;
+  messageType;
+}>`
   position: absolute;
   display: flex;
   flex-direction: row;
   justify-content: center;
   background-color: ${({
+    messageType,
     theme: {
-      popups: {
-        states: { error },
-      },
+      popups: { states },
     },
-  }) => error.backgroundColor};
+  }) => states[messageType].backgroundColor};
   ${({ theme }) => `border-radius: ${theme.box.radius}px;`}
 `;
 
@@ -33,29 +35,36 @@ const CloseImg = styled.img.attrs((props) => ({
   }) => iconRadius}px;
 `;
 
-const PopupText = styled.div`
+const PopupText = styled.div<{
+  messageType;
+}>`
   margin: 10px 10px 10px 30px;
   font-size: 10px;
   line-height: 13px;
   color: ${({
+    messageType,
     theme: {
-      popups: {
-        states: { error },
-      },
+      popups: { states },
     },
-  }) => error.color};
+  }) => states[messageType].color};
 `;
 
 type Props = {
   onClick: () => void;
   text: string;
   style?: any;
+  messageType?: 'error' | 'warning' | 'infoTooltip' | 'success';
 };
 
-const ErrorPopup = ({ onClick, text, style = {} }: Props) => (
-  <Wrapper onClick={onClick} style={style}>
+const ErrorPopup = ({
+  onClick,
+  text,
+  style = {},
+  messageType = 'error',
+}: Props) => (
+  <Wrapper onClick={onClick} style={style} messageType={messageType}>
     <CloseImg src={closePopup} />
-    <PopupText>{text}</PopupText>
+    <PopupText messageType={messageType}>{text}</PopupText>
   </Wrapper>
 );
 
