5f8ee548bc578e40eb3d5957b1cdf3a0e0c9a5b9
brusher_ru <brusher.designer@gmail.com>
2023-08-01 22:16:13 +0400

    tweak: improve typing

--- shared/deepMergeOmitNulls.ts
+++ shared/deepMergeOmitNulls.ts
@@ -1,9 +1,13 @@
 import { compose, mergeWith, omit } from 'ramda';
 import { isObject } from './utils';
 
+type DeppAny<T> = T extends Record<string, any>
+  ? Partial<{ [K in keyof T]: DeppAny<T[K]> | null }> & { [k: string]: any }
+  : T;
+
 const deepMergeOmitNulls = <T extends Record<string, any>>(
   prev: T,
-  next: Partial<T>
+  next: DeppAny<T>
 ): T => {
   const nextEntries = Object.entries(next);
   const nulls = nextEntries.filter(([_, v]) => v === null).map(([k]) => k);
