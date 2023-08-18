b1fe51c853de39a3f435adb843a9d91a62d47bda
maxym <maksim.parfenov@protonmail.ch>
2023-08-18 15:41:48 +0100

    feature: add update proving opts on the edit page

---
+++ app/screens/modal/PoSProvingOptsUpdateModal.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PoSProfiler from '../../components/node/PoSProfiler';
+import { Modal } from '../../components/common';
+
+import useUpdatePostProvingOpts from '../../hooks/useUpdatePostProvingOpts';
+import ReactPortal from './ReactPortal';
+
+interface Props {
+  closeHandler: () => void;
+}
+const PoSProvingOptsUpdateModal = ({ closeHandler }: Props) => {
+  const {
+    updateConfigHandler,
+    numUnits,
+    dataDir,
+    singleCommitmentSize,
+    loading,
+    nonces,
+    threads,
+  } = useUpdatePostProvingOpts(closeHandler);
+
+  return (
+    <ReactPortal modalId="pos-profiler-modal">
+      <Modal
+        header={'Update PoS proving opts'}
+        subHeader={`This screen allows updating the number of Nonces and CPU threads. 
+          These values will be used in the proving process.`}
+        width={750}
+        height={530}
+      >
+        <PoSProfiler
+          nextAction={updateConfigHandler}
+          numUnitSize={singleCommitmentSize}
+          maxUnits={numUnits}
+          dataDir={dataDir}
+          threads={threads}
+          nonces={nonces}
+          posFooterProps={{
+            isDisabled: loading,
+            nextLabel: loading ? ' Updating...' : 'Update',
+            skipLabel: 'Close',
+            skipAction: loading ? undefined : closeHandler,
+          }}
+        />
+      </Modal>
+    </ReactPortal>
+  );
+};
+
+export default PoSProvingOptsUpdateModal;
