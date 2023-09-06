1361b61823b8827d99771131fb88008c9b757bb6
brusher_ru <brusher.designer@gmail.com>
2023-09-06 18:55:46 +0400

    Revert "fix: get rid of excessive handler on wallet change"
    This reverts commit cabb672b5521706b7114e72621725e17628e133e.

---
+++ desktop/main/reactions/handleAppWalletChange.ts
@@ -0,0 +1,28 @@
+import * as $ from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { makeSubscription } from '../rx.utils';
+import { isLocalNodeType } from '../../../shared/utils';
+import { Wallet } from '../../../shared/types';
+import { Managers } from '../app.types';
+
+export default (
+  $isWalletActivated: Subject<void>,
+  $wallet: Observable<Wallet | null>,
+  $managers: Subject<Managers>
+) =>
+  makeSubscription(
+    $isWalletActivated.pipe($.withLatestFrom($managers, $wallet)),
+    ([_, managers, wallet]) => {
+      const type = wallet?.meta?.type;
+
+      if (!type || !isLocalNodeType(type)) {
+        return;
+      }
+
+      (async () => {
+        await managers.node.startNode();
+        await managers.node.updateNodeStatus();
+        await managers.smesher.serviceStartupFlow();
+      })();
+    }
+  );
