eaf0484ac73b0cd6e50d13b524e53a8e534f6536
brusher_ru <brusher.designer@gmail.com>
2023-10-04 15:45:02 +0400

    refactor: remove useless code

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,6 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
 
-const logger = require('electron-log');
-
-logger.transports.console.level = false;
-
 const root = ReactDom.createRoot(
   document.getElementById('root') as HTMLElement
 );
