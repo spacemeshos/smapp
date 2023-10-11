6bea2f75b2c4c6334318fe3305c5f06cb1d20ffa
brusher_ru <brusher.designer@gmail.com>
2023-10-11 16:01:11 +0400

    fix: sync autostart value from config with OS on start and handle toggling properly

--- desktop/AutoStartManager.ts
+++ desktop/AutoStartManager.ts
@@ -1,121 +1,122 @@
 import { ipcMain } from 'electron';
 import AutoLaunch from 'auto-launch';
-import { captureException } from '@sentry/electron';
 import { ipcConsts } from '../app/vars';
 import { isLinuxAppImage } from '../shared/utils';
 import StoreService from './storeService';
-
 import { isMacOS } from './osSystem';
+import Logger from './logger';
+import { captureMainException } from './sentry';
 
-export const IS_AUTO_START_ENABLED = 'isAutoStartEnabled';
+// Results
 
 type ToggleResult = {
   status: boolean;
   error?: string;
 };
 
-class AutoStartManager {
-  private manager: AutoLaunch;
+const handleFailure = (err: unknown): ToggleResult => {
+  if (
+    isMacOS() &&
+    err instanceof Error &&
+    err.message &&
+    /System Events/.test(err.message)
+  ) {
+    return {
+      status: false,
+      error:
+        'Can not setup auto start: you need to provide permissions.\nGo to Settings -> Security & Privacy -> Privacy -> Automation and mark the checkbox next to "System Events" for the Spacemesh app.\nAnd then try again.',
+    };
+  }
 
-  constructor() {
-    const options: { name: string; isHidden: boolean; path?: string } = {
+  err instanceof Error && captureMainException(err);
+
+  return {
+    status: false,
+    error: `Can not setup auto launch: ${err}`,
+  };
+};
+
+// Utils
+
+const IS_AUTO_START_ENABLED = 'isAutoStartEnabled';
+const storeValue = (val: boolean) =>
+  StoreService.set(IS_AUTO_START_ENABLED, val);
+const loadValue = () => StoreService.get(IS_AUTO_START_ENABLED);
+
+const logger = Logger({ className: 'AutoStartManager' });
+
+// Singletone class
+class AutoStartManager {
+  static service: AutoLaunch;
+
+  static init() {
+    logger.debug('init() called...');
+    AutoStartManager.service = new AutoLaunch({
       name: 'Spacemesh',
       isHidden: true,
-    };
-
-    // Linux issue with path to the application
-    if (isLinuxAppImage()) {
-      options.path = process.env.APPIMAGE;
-    }
-
-    this.manager = new AutoLaunch(options);
-
-    if (this.isEnabled()) {
-      this.enable();
-    }
+      path: isLinuxAppImage() ? process.env.APPIMAGE : process.execPath,
+    });
 
     ipcMain.removeAllListeners(ipcConsts.TOGGLE_AUTO_START);
-    ipcMain.handle(ipcConsts.TOGGLE_AUTO_START, async () =>
-      this.toggleAutoStart()
+    ipcMain.handle(ipcConsts.TOGGLE_AUTO_START, () =>
+      AutoStartManager.toggleAutoStart()
     );
 
     ipcMain.removeHandler(ipcConsts.IS_AUTO_START_ENABLED_REQUEST);
     ipcMain.handle(ipcConsts.IS_AUTO_START_ENABLED_REQUEST, () =>
-      this.isEnabled()
+      AutoStartManager.isEnabled()
     );
+
+    // Syncronise value from persistent store with system auto-launch
+    AutoStartManager.isEnabled()
+      ? AutoStartManager.enable()
+      : AutoStartManager.disable();
   }
 
-  toggleAutoStart = async () => {
-    return StoreService.get(IS_AUTO_START_ENABLED)
-      ? this.disable()
-      : this.enable();
-  };
+  static isEnabled = () => loadValue();
 
-  isEnabled = () => {
-    return StoreService.get(IS_AUTO_START_ENABLED);
-  };
+  static toggleAutoStart = () =>
+    AutoStartManager.isEnabled()
+      ? AutoStartManager.disable()
+      : AutoStartManager.enable();
 
-  disable = async (): Promise<ToggleResult> => {
+  static disable = async (n = 0): Promise<ToggleResult> => {
     try {
-      const isEnabled = await this.manager.isEnabled();
-      if (isEnabled) {
-        await this.manager.disable();
-        return await this.disable();
+      const isEnabled = await AutoStartManager.service.isEnabled();
+      if (isEnabled && n === 0) {
+        await AutoStartManager.service.disable();
+        return await AutoStartManager.disable(n + 1);
+      } else if (isEnabled) {
+        throw new Error('Cannot disable auto-launch for unknown reason');
       } else {
-        StoreService.set(IS_AUTO_START_ENABLED, false);
+        storeValue(false);
         return {
           status: false,
         };
       }
     } catch (err) {
-      StoreService.set(IS_AUTO_START_ENABLED, false);
-      if (
-        isMacOS() &&
-        err instanceof Error &&
-        err.message &&
-        /System Events/.test(err.message)
-      ) {
-        // Since User disables the feature it does not matter
-        // do we have permissions or not — just "turn it off"
-        return { status: false };
-      }
-
-      captureException(err);
-      return {
-        status: false,
-        error: `Can not setup auto launch: ${err}`,
-      };
+      logger.error('disable()', err);
+      return handleFailure(err);
     }
   };
 
-  enable = async (): Promise<ToggleResult> => {
+  static enable = async (n = 0): Promise<ToggleResult> => {
     try {
-      const isEnabled = await this.manager.isEnabled();
-      if (!isEnabled) {
-        await this.manager.enable();
-        return await this.enable();
+      const isEnabled = await AutoStartManager.service.isEnabled();
+      if (!isEnabled && n === 0) {
+        await AutoStartManager.service.enable();
+        return await AutoStartManager.enable(n + 1);
+      } else if (!isEnabled) {
+        throw new Error(
+          'Cannot enable auto-launch. Probably you already have another Spacemesh application in auto-launch'
+        );
       } else {
-        StoreService.set(IS_AUTO_START_ENABLED, true);
+        storeValue(true);
         return { status: true };
       }
     } catch (err) {
-      if (
-        isMacOS() &&
-        err instanceof Error &&
-        err.message &&
-        /System Events/.test(err.message)
-      ) {
-        return {
-          status: false,
-          error:
-            'Can not setup auto start: you need to provide permissions.\nGo to Settings -> Security & Privacy -> Privacy -> Automation and mark the checkbox next to "System Events" for the Spacemesh app.\nAnd then try again.',
-        };
-      }
-      captureException(err);
-      return {
-        status: false,
-        error: `Can not setup auto launch: ${err}`,
-      };
+      logger.error('enable()', err);
+      return handleFailure(err);
     }
   };
 }
