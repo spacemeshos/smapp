89bf217f0731a36fcae4634a4e265743052a7da2
brusher_ru <brusher.designer@gmail.com>
2023-11-22 22:34:31 +0700

    fix: get rid of useless $nodeConfig argument

--- desktop/main/reactions/handleSmesherIpc.ts
+++ desktop/main/reactions/handleSmesherIpc.ts
@@ -1,10 +1,6 @@
 import { from, Subject, switchMap, withLatestFrom } from 'rxjs';
 import { ipcConsts } from '../../../app/vars';
-import {
-  NodeConfig,
-  PostProvingOpts,
-  PostSetupOpts,
-} from '../../../shared/types';
+import { PostProvingOpts, PostSetupOpts } from '../../../shared/types';
 import { SmeshingSetupState } from '../../NodeManager';
 import Logger from '../../logger';
 import { Managers } from '../app.types';
@@ -15,21 +11,19 @@ const logger = Logger({ className: 'handleSmesherIpc' });
 const startSmeshing = (
   managers: Managers,
   opts: PostSetupOpts,
-  provingOpts: PostProvingOpts,
-  $nodeConfig: Subject<NodeConfig>
-) => wrapResult(managers.node.startSmeshing(opts, provingOpts, $nodeConfig));
+  provingOpts: PostProvingOpts
+) => wrapResult(managers.node.startSmeshing(opts, provingOpts));
 
 export default (
   $managers: Subject<Managers>,
-  $smeshingSetupState: Subject<SmeshingSetupState>,
-  $nodeConfig: Subject<NodeConfig>
+  $smeshingSetupState: Subject<SmeshingSetupState>
 ) => {
   const startSmeshingRequest = fromIPC<[PostSetupOpts, PostProvingOpts]>(
     ipcConsts.SMESHER_START_SMESHING
   ).pipe(
     withLatestFrom($managers),
     switchMap(([[opts, provingOpts], managers]) =>
-      from(startSmeshing(managers, opts, provingOpts, $nodeConfig))
+      from(startSmeshing(managers, opts, provingOpts))
     )
   );
 
