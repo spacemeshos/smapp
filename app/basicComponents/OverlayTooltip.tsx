4b1c56a3c328a903932a3e3734fa2fef3b55f416
maxym <maksim.parfenov@protonmail.ch>
2023-11-23 00:05:52 +0100

    tweak: update OverlayTooltip styles

--- app/basicComponents/OverlayTooltip.tsx
+++ app/basicComponents/OverlayTooltip.tsx
@@ -1,42 +1,66 @@
 import React from 'react';
 import { PlacesType, Tooltip, VariantType } from 'react-tooltip';
 import styled from 'styled-components';
+import { smColors } from '../vars';
 
 interface OverlayTooltipProps {
-  id: string;
+  anchorClassName: string;
   content: string;
   place?: PlacesType;
   variant?: VariantType;
 }
 
 const StyledTooltip = styled(Tooltip)`
-  background-color: #555 !important;
-  color: #fff !important;
-  border-radius: 4px !important;
-  padding: 8px 12px !important;
-  font-size: 14px !important;
   max-width: 300px !important;
+  opacity: 1;
+  padding: 10px !important;
+  background-color: ${({
+    theme: {
+      popups: {
+        states: { infoTooltip },
+      },
+    },
+  }) => infoTooltip.backgroundColor} !important;
+  border: 1px solid ${smColors.realBlack};
+  border-radius: ${({
+    theme: {
+      popups: { boxRadius },
+    },
+  }) => boxRadius}px !important;
+  font-size: 10px !important;
+  line-height: 13px;
+
+  white-space: pre-wrap;
+  color: ${({
+    theme: {
+      popups: {
+        states: { infoTooltip },
+      },
+    },
+  }) => infoTooltip.color} !important;
 `;
 
 const OverlayTooltip = ({
-  id,
+  anchorClassName,
   content,
   place = 'top',
   variant,
 }: OverlayTooltipProps) => {
   return (
     <StyledTooltip
-      anchorSelect={`.${id}`}
+      anchorSelect={`.${anchorClassName}`}
       place={place}
       content={content}
       variant={variant}
+      noArrow
+      opacity={1}
     />
   );
 };
 
 interface OverlayTooltipListProps
-  extends Omit<OverlayTooltipProps, 'id' | 'content'> {
-  list: Array<{ id: string; content: string }>;
+  extends Omit<OverlayTooltipProps, 'anchorClassName' | 'content'> {
+  list: Array<{ anchorClassName: string; content: string }>;
 }
 
 const OverlayTooltipList = ({
@@ -47,7 +71,7 @@ const OverlayTooltipList = ({
   <>
     {list.map((tooltipContent) => (
       <OverlayTooltip
-        key={tooltipContent.id}
+        key={tooltipContent.anchorClassName}
         {...tooltipContent}
         place={place}
         variant={variant}
